94101ab8b87b6d95a770d26129505038fb8bbe42
Daniel Hnyk <hnykda@gmail.com>
2024-08-31 11:08:34 +0200

    adding app shell

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+
+import { ActionBar } from "@/lib/components/ActionBar/ActionBar";
+import { Navbar } from "@/lib/components/Navbar/Navbar";
+import { AppShell, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+
 export default function Home() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: "auto",
+        breakpoint: "sm",
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        <div>Logo</div>
+      </AppShell.Header>
+
+      <AppShell.Navbar>
+        <Navbar />
+      </AppShell.Navbar>
+
+      <AppShell.Main>Main</AppShell.Main>
+      <AppShell.Aside>
+        <ActionBar />
+      </AppShell.Aside>
+    </AppShell>
   );
 }
---
+++ app/src/lib/components/Navbar/Navbar.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { Center, Tooltip, UnstyledButton, Stack, rem } from "@mantine/core";
+import {
+  IconHome2,
+  IconGauge,
+  IconDeviceDesktopAnalytics,
+  IconFingerprint,
+  IconCalendarStats,
+  IconUser,
+  IconSettings,
+  IconLogout,
+  IconSwitchHorizontal,
+} from "@tabler/icons-react";
+import classes from "./Navbar.module.css";
+
+interface NavbarLinkProps {
+  icon: typeof IconHome2;
+  label: string;
+  active?: boolean;
+  onClick?(): void;
+}
+
+function NavbarLink({ icon: Icon, label, active, onClick }: NavbarLinkProps) {
+  return (
+    <Tooltip label={label} position="right" transitionProps={{ duration: 0 }}>
+      <UnstyledButton
+        onClick={onClick}
+        className={classes.link}
+        data-active={active || undefined}
+      >
+        <Icon style={{ width: rem(20), height: rem(20) }} stroke={1.5} />
+      </UnstyledButton>
+    </Tooltip>
+  );
+}
+
+const mockdata = [
+  { icon: IconHome2, label: "Home" },
+  { icon: IconGauge, label: "Projects" },
+  { icon: IconDeviceDesktopAnalytics, label: "Analytics" },
+];
+
+export function Navbar() {
+  const [active, setActive] = useState(2);
+
+  const links = mockdata.map((link, index) => (
+    <NavbarLink
+      {...link}
+      key={link.label}
+      active={index === active}
+      onClick={() => setActive(index)}
+    />
+  ));
+
+  return (
+    <>
+      <div className={classes.navbarMain}>
+        <Stack justify="center" gap={0}>
+          {links}
+        </Stack>
+      </div>
+
+      <Stack justify="center" gap={0}>
+        <NavbarLink icon={IconSwitchHorizontal} label="Change account" />
+        <NavbarLink icon={IconLogout} label="Logout" />
+      </Stack>
+    </>
+  );
+}
